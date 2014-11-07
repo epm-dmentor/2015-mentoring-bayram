f1263bd3320980b511d8462e55d5f27e55001ed2
bayramchik <bayramchik@gmail.com>
2014-11-07 13:51:01 +0200

    IDisposable task implementation

---
+++ practice/IDisposable/MentoringLesson1/MentoringLesson1/Logger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentoringLesson1
+{
+    public class MemoryStreamLogger:ILogger,IDisposable
+    
+    {
+        private FileStream memoryStream;
+        private StreamWriter streamWriter;
+        private bool disposed;
+        
+        public MemoryStreamLogger()
+        {
+            this.memoryStream = new FileStream(@"D:\log.txt",FileMode.OpenOrCreate);
+            this.streamWriter = new StreamWriter(memoryStream);
+        }
+
+        public void Log(string message)
+        {
+          
+            streamWriter.Write(message);
+            streamWriter.Flush();
+        }
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            if (disposing)
+            {
+                if (memoryStream!=null && streamWriter!=null)
+                {
+                    
+                    streamWriter.Dispose();
+                    //Console.WriteLine("Stream Writer Disposed");
+                    memoryStream.Dispose();
+                    //Console.WriteLine("Memory Stream Disposed");
+                    memoryStream =null;
+                    streamWriter = null;
+
+                }
+
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~MemoryStreamLogger()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ practice/IDisposable/MentoringLesson1/MentoringLesson1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentoringLesson1
+{
+    class ExtendedFileWriter:FileStream
+    {
+        public ExtendedFileWriter(string FileName,FileMode mode):base(FileName,mode)
+        {
+           
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (var logger = new MemoryStreamLogger())
+            {
+            
+                logger.Log("Test");
+                logger.Log("Test2");
+               
+            }
+            
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+            
+        }
+    }
+}
