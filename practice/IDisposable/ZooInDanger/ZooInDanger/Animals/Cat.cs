247a790a3774a85c858d565423d6da90104974fd
bayramo <bayramchik@gmail.com>
2014-11-20 10:08:07 +0200

    Finalizer was fixed in Cat class

--- practice/IDisposable/ZooInDanger/ZooInDanger/Animals/Cat.cs
+++ practice/IDisposable/ZooInDanger/ZooInDanger/Animals/Cat.cs
@@ -28,7 +28,6 @@ namespace Zoo.Animals
             while (Zoo.Troops > 100)
             {
                 Interlocked.Decrement(ref Zoo.Troops);
-                
             }
         }
     }
