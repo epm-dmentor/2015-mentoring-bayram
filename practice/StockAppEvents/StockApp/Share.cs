df6a2f1822be113553163b28fb5ccbd0d9fe92c3
bayramo <bayramchik@gmail.com>
2014-11-20 10:57:22 +0200

    Stock App with Events

---
+++ practice/StockAppEvents/StockApp/Share.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp
+{
+    public class Share
+    {
+        public string ShareName { get; set; }
+        public double SharePrice { get; set; }
+    }
+}
