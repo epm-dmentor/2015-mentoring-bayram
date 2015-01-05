e4212cb02015d4a76df9a521fd7d5b9ef92aaaf8
bayramo <bayramchik@gmail.com>
2015-01-05 15:46:33 +0200

    Added CalculationService

---
+++ practice/CalculatorDecorator/CalculatorDecorator/CalculationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalculatorDecorator
+{
+    public class CalculationService
+    {
+        private Dictionary<Tuple<decimal,decimal>,decimal> _cache;
+
+        public CalculationService()
+        {
+            _cache = new Dictionary<Tuple<decimal, decimal>, decimal>();
+        }
+
+        protected decimal CheckCache(decimal a, decimal b)
+        {
+            var inputParameters = new Tuple<decimal, decimal>(a,b);
+            
+            if (_cache.ContainsKey(inputParameters))
+                return _cache[inputParameters];
+            
+            var value = a*a + 2*a*b*b*b;
+            _cache.Add(inputParameters,value);
+            return value;
+
+        }
+        
+        public decimal Calculate(decimal a, decimal b)
+        {
+
+            Thread.Sleep(1000);
+            return CheckCache(a, b);
+        }
+
+
+
+    }
+}
--- practice/CalculatorDecorator/CalculatorDecorator/Program.cs
+++ practice/CalculatorDecorator/CalculatorDecorator/Program.cs
@@ -6,13 +6,6 @@ namespace CalculatorDecorator
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            var result =
-                new Add(new Multiply(new Const(12), new Const(3)),
-                    new Plus(new Const(2), new Divide(
-                             new Const(12), new Const(3))))
-                             .GetResult();
-=======
 
             var result = 
                 new Add(
@@ -24,9 +17,19 @@ namespace CalculatorDecorator
                             new Divide(
                                 new Const(12),
                                 new Const(3)))).GetResult();
->>>>>>> 27c82def8acb8fc84260fec7506fe2994e1bda83
+
             
             Console.WriteLine(result);
+
+
+            var calc = new CalculationService();
+
+            var res1 = calc.Calculate(23, 23);
+            var res2 = calc.Calculate(45, 45);
+            var res3 = calc.Calculate(23, 23);
+
+            Console.WriteLine("Result one - {0}, Result two {1}, Result from cache {2}",res1,res2,res3);
+
             Console.ReadKey();
         }
     }
