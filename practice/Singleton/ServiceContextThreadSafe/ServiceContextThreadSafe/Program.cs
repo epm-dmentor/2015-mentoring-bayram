d13cd39832cad6f37d072769e94cb8ee490ce158
bayramo <bayramchik@gmail.com>
2015-01-29 16:45:46 +0200

    Thread Safe Implementation of Singleton Pattern

---
+++ practice/Singleton/ServiceContextThreadSafe/ServiceContextThreadSafe/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Epam.NetMentoring.ServiceContextThreadSafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = ServiceContext.Instance();
+            service.ConnectionUrl = "http://127.0.0.1";
+            service.Port = 8080;
+            service.Timeout = 10000;
+
+            var service2 = ServiceContext.Instance();
+        }
+    }
+}
---
+++ practice/Singleton/ServiceContextThreadSafe/ServiceContextThreadSafe/ServiceContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+
+namespace Epam.NetMentoring.ServiceContextThreadSafe
+{
+    internal sealed class ServiceContext
+    {
+        private static readonly object Lock = new object();
+        private static ServiceContext _instance;
+
+        public string ConnectionUrl { get; set; }
+        public int Port { get; set; }
+        public int Timeout { get; set; }
+
+        private ServiceContext() {}
+
+        public static ServiceContext Instance()
+        {
+            if (_instance != null) return _instance;
+            Monitor.Enter(Lock);
+            {
+                var tempContext = new ServiceContext();
+                Volatile.Write(ref _instance,tempContext);
+            }
+            Monitor.Exit(Lock);
+            return _instance;
+        }
+    }
+}
