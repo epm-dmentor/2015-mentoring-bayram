0f602975503b39bc6261f802fc445e03391112ff
Igor Tkachenko <legart.ua@gmail.com>
2015-01-29 14:01:09 +0200

    Added unit tests

---
+++ practice/DataStructures/LinkedList/LinkedListTests/LinkedListBaseTests.cs
@@ -0,0 +1,37 @@
+﻿using Epam.NetMentoring.LinkedList;
+using NUnit.Framework;
+using System;
+using System.Drawing;
+
+namespace LinkedListTests
+{
+    [TestFixture]
+    public class LinkedListBaseTests
+    {
+        [Test]
+        public void ThrowExceptionInCaseInsertingToIncorrectPosition()
+        {
+            var linkedList = new LinkedList();
+            Assert.Throws<Exception>(() => linkedList.InsertAt(new Point(1, 2), -1));
+        }
+
+        [Test]
+        public void CheckGetElementByIndex()
+        {
+            var linkedList = new LinkedList();
+            var newElement = new Point(1, 2);
+            linkedList.Add(newElement);
+            var elFromList = linkedList.ElementAt(0);
+            Assert.That(elFromList, Is.EqualTo(newElement));
+        }
+
+        [Test]
+        public void CheckOutOfRangeElementBy()
+        {
+            var linkedList = new LinkedList();
+            var newElement = new Point(1, 2);
+            linkedList.Add(newElement);
+            Assert.Throws<ArgumentOutOfRangeException>(() => linkedList.ElementAt(10));
+        }
+    }
+}
