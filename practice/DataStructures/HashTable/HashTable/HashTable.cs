1f15394235ab513d67ec899ed3bb47e317b6d59d
Igor Tkachenko <legart.ua@gmail.com>
2015-02-26 14:39:49 +0200

    reviewd

--- practice/DataStructures/HashTable/HashTable/HashTable.cs
+++ practice/DataStructures/HashTable/HashTable/HashTable.cs
@@ -4,19 +4,22 @@ using System.Linq;
 
 namespace Epam.NetMentoring.HashTable
 {
-    public class HashTable:IHashTable
+    public class HashTable : IHashTable
     {
+        //bucket size is const. Should fetch to envrironment automaticly
         private const int _size = 128;
-        private readonly List<HashItem>[] table;
+        //List<HashTableBucket>
+        //List<Bucket>
+        private readonly List<HashItem>[] table = new List<HashItem>[_size];
 
-        public HashTable()
-        {
-            table = new List<HashItem>[_size];
-        }
+        //private int CompressHashCode(int hashCode)
+        //private int Compress(int hashCode)
+
 
         private static int GetHash(WordEntity key)
         {
-            return Math.Abs(key.Word.Aggregate(7, (current, t) => current*31 + t)%128);
+            //Must be calculated inside an object
+            return Math.Abs(key.Word.Aggregate(7, (current, t) => current * 31 + t) % 128);
         }
 
         public bool Contains(WordEntity key)
@@ -31,18 +34,19 @@ namespace Epam.NetMentoring.HashTable
 
             var node = new HashItem(key, value);
             var hash = GetHash(key);
-            
+
             for (var i = 0; i < _size; i++)
             {
                 if (i == hash && table[hash] == null)
                 {
-                    table[hash] = new List<HashItem> {node};
+                    table[hash] = new List<HashItem> { node };
                 }
-                else if (i==hash && table[hash]!=null)
-                table[hash].Add(node);
-                
+                else if (i == hash && table[hash] != null)
+
+                    table[hash].Add(node);
+
             }
-           
+
         }
 
         public WordDefinition Get(WordEntity key)
@@ -50,8 +54,12 @@ namespace Epam.NetMentoring.HashTable
             var hash = GetHash(key);
 
             if (table[hash] == null) throw new Exception("Cannot retrieve value from given key " + key.Word);
-            var res = table[hash].Where(x => x.Key.Equals(key)).Select(x => x.Value).FirstOrDefault();
-            
+            var res = 
+                table[hash]
+                    .Where(x => x.Key.Equals(key))
+                    .Select(x => x.Value)
+                    .FirstOrDefault();
+
             return res;
         }
 
